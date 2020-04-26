998a8dcde97aac252893865c69b72e6c1117e1eb
hb432 <hb432@protonmail.com>
2020-04-26 18:31:54 -0400

    no minify

--- src/main/java/xyz/grakkit/index.mjs
+++ src/main/java/xyz/grakkit/index.mjs
@@ -1,56 +1,72 @@
 // BEGIN BASIC
 const _ = globalThis;
-(_.global = _),
-   (_.server = org.bukkit.Bukkit.getServer()),
-   (_.plugin = _.server.getPluginManager().getPlugin('Grakkit')),
-   (_.event = (a) => ({
-      on: (b, c) => (a[b] || (a[b] = [])).push(c),
-      fire: (b, ...c) => (a[b] || (a[b] = [])).map((a) => a(...c))
-   })),
-   (_.command = (a, b) => {
-      b || (b = {});
-      const c = b.usage,
-         d = b.desc,
-         e = b.error,
-         f = b.aliases || [],
-         g = (b.executor || (() => {})).toString(),
-         h = (b.tabCompleter || (() => [])).toString(),
-         i = b.prefix || 'grakkit';
-      return _.plugin.command(a, c, d, e, f, g, h).register(i);
-   }),
-   (_.bukkit = _.event({})),
-   (_.on = (event, listener) => {
-      const length = _.bukkit.on(event, listener);
-      return (
-         1 === length &&
-            _.server
-               .getPluginManager()
-               .registerEvent(
-                  eval(event).class,
-                  new (Java.extend(org.bukkit.event.Listener, {}))(),
-                  org.bukkit.event.EventPriority.HIGHEST,
-                  (a, b) => _.fire(event, b),
-                  plugin
-               ),
-         length
-      );
-   }),
-   (_.fire = (...a) => _.bukkit.fire(...a)),
-   (_.tabCompleter = (a) => {
-      const b = /.*(\!|\^|\&|\*|\(|\-|\+|\=|\[|\{|\||\;|\:|\,|\?|\/)/,
-         c = a.replace(b, '').split('.');
-      let d = global,
-         e = 0;
-      for (; e < c.length - 1; ) {
-         let a = c[e];
-         d[a] ? ((d = d[a]), ++e) : (e = 1 / 0);
+
+_._ = _;
+_.server = org.bukkit.Bukkit.getServer();
+_.plugin = _.server.getPluginManager().getPlugin('Grakkit');
+
+_.event = (store) => {
+   return {
+      on: (event, listener) => {
+         return (store[event] || (store[event] = [])).push(listener);
+      },
+      fire: (event, ...data) => {
+         return (store[event] || (store[event] = [])).map((listener) => listener(...data));
       }
-      if (e === c.length - 1) {
-         const e = c.slice(-1)[0];
-         return Object.getOwnPropertyNames(d)
-            .filter((a) => a.includes(e))
-            .map((d) => (a.match(b) || [ '' ])[0] + [ ...c.slice(0, -1), d ].join('.'));
+   };
+};
+
+_.command = (name, options) => {
+   options || (options = {});
+   const usage = options.usage;
+   const desc = options.desc;
+   const error = options.error;
+   const aliases = options.aliases || [];
+   const executor = (options.executor || (() => {})).toString();
+   const tabCompleter = (options.tabCompleter || (() => [])).toString();
+   const prefix = options.prefix || 'grakkit';
+   return _.plugin.command(name, usage, desc, error, aliases, executor, tabCompleter).register(prefix);
+};
+
+_.bukkit = _.event({});
+_.on = (event, listener) => {
+   const length = _.bukkit.on(event, listener);
+   if (length === 1) {
+      _.server
+         .getPluginManager()
+         .registerEvent(
+            eval(event).class,
+            new (Java.extend(org.bukkit.event.Listener, {}))(),
+            org.bukkit.event.EventPriority.HIGHEST,
+            (info, data) => _.fire(event, data),
+            plugin
+         );
+   }
+   return length;
+};
+_.fire = (...args) => _.bukkit.fire(...args);
+
+_.tabCompleter = (input) => {
+   const filter = /.*(\!|\^|\&|\*|\(|\-|\+|\=|\[|\{|\||\;|\:|\,|\?|\/)/;
+   const nodes = input.replace(filter, '').split('.');
+   let context = _;
+   let index = 0;
+   while (index < nodes.length - 1) {
+      let node = nodes[index];
+      if (context[node]) {
+         context = context[node];
+         ++index;
+      } else {
+         index = Infinity;
       }
+   }
+   if (index === nodes.length - 1) {
+      const segment = nodes.slice(-1)[0];
+      return Object.getOwnPropertyNames(context)
+         .filter((key) => key.includes(segment))
+         .map((comp) => (input.match(filter) || [ '' ])[0] + [ ...nodes.slice(0, -1), comp ].join('.'));
+   } else {
       return [];
-   });
+   }
+};
 // END BASIC
