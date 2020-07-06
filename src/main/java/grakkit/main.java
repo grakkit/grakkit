f200d56da03a99272214e28c12cee9c45e7f5e35
hb432 <hb432@protonmail.com>
2020-07-06 16:01:41 -0400

    boop!

---
+++ src/main/java/grakkit/main.java
@@ -0,0 +1,37 @@
+package grakkit;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Source;
+
+public final class main extends JavaPlugin {
+
+   public static Context context;
+
+   @Override
+   public void onEnable() {
+      try {
+         Paths.get("plugins/grakkit").toFile().mkdir();
+         File local = Paths.get("plugins/grakkit/index.js").toFile();
+         if (!local.exists()) {
+            Files.createFile(local.toPath());
+            URL remote = new URL("https://raw.githubusercontent.com/grakkit/grakkit/master/index.min.js");
+            remote.openStream().transferTo(new FileOutputStream(local));
+         }
+         main.context = Context.newBuilder("js").allowAllAccess(true).build();
+         try {
+            main.context.eval(Source.newBuilder("js", local).cached(false).build());
+         } catch (Exception error) {
+            error.printStackTrace(System.err);
+         }
+      } catch (Exception error) {
+         error.printStackTrace(System.err);
+         this.getServer().getPluginManager().disablePlugin(this);
+      }
+   }
+}
