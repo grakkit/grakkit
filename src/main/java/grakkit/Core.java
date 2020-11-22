2d6418b35946d7d4dddc0d033c6bab0b54e56676
hb432 <hb432@protonmail.com>
2020-11-22 14:28:51 -0500

    consolidation, expansion, and innovation

--- src/main/java/grakkit/Core.java
+++ src/main/java/grakkit/Core.java
@@ -1,6 +1,9 @@
 package grakkit;
 
 import java.io.File;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.nio.file.Paths;
 import java.util.LinkedList;
 import java.util.List;
@@ -9,9 +12,64 @@ import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.Source;
 import org.graalvm.polyglot.Value;
 
-public final class Core {
+public class Core {
 
+   static {
+
+      // handle errors
+      try {
+
+         // check if graal is installed
+         Class.forName("org.graalvm.polyglot.Value");
+      } catch (Exception $) {
+
+         // handle errors
+         try {
+
+            // load classes from plugin
+            URLClassLoader loader = (URLClassLoader) Thread.currentThread().getContextClassLoader();
+            Class<URLClassLoader> clazz = URLClassLoader.class;
+            Method method = clazz.getDeclaredMethod("addURL", URL.class);
+            method.setAccessible(true);
+            method.invoke(loader, Core.locate(Core.class));
+         } catch (Exception error) {
+
+            // throw error
+            throw new RuntimeException("Failed to add plugin to class path!", error);
+         }
+      }
+   }
+
+   private static URL locate (Class<?> clazz) {
+      try {
+         URL location = clazz.getProtectionDomain().getCodeSource().getLocation();
+         if (location instanceof URL) return location;
+      } catch (SecurityException | NullPointerException error) {
+         // try other method instead of throwing error
+      }
+      URL resource = clazz.getResource(clazz.getSimpleName() + ".class");
+      if (resource instanceof URL) {
+         String link = resource.toString();
+         String suffix = clazz.getCanonicalName().replace('.', '/') + ".class";
+         if (link.endsWith(suffix)) {
+            String base = link.substring(0, link.length() - suffix.length()), path = base;
+            if (path.startsWith("jar:")) path = path.substring(4, path.length() - 2);
+            try {
+               return new URL(path);
+            } catch (Exception error) {
+               return null;
+            }
+         } else {
+            return null;
+         }
+      } else {
+         return null;
+      }
+   }
+
+   public static String root;
    public static Context context;
+   public static List<Value> hooks = new LinkedList<>();
    public static List<Value> queue = new LinkedList<>();
 
    public static void loop () {
@@ -21,13 +79,13 @@ public final class Core {
       });
    }
 
-   public static void load (String path, String... more) throws Exception {
-      
-      // close context to prepare for new one
-      if (Core.context instanceof Context) Core.context.close();
+   public static void open (String path, String main) throws Exception {
+
+      // declare root path
+      Core.root = path;
 
       // get index file from config
-      File index = Paths.get(path, more).toFile();
+      File index = Paths.get(path, main).toFile();
 
       // check if index exists
       if (index.exists()) {
@@ -38,7 +96,7 @@ public final class Core {
             .allowExperimentalOptions(true)
             .option("js.nashorn-compat", "true")
             .option("js.commonjs-require", "true")
-            .option("js.commonjs-require-cwd", "./plugins/grakkit")
+            .option("js.commonjs-require-cwd", path)
             .build();
 
          // evaluate index
@@ -57,11 +115,45 @@ public final class Core {
       }
    }
 
-   public void queue (Value script) {
+   public static void close () {
+   
+      // trigger unload hooks
+      new LinkedList<Value>(Core.hooks).forEach(value -> {
+         value.execute();
+         Core.hooks.remove(value);
+      });
+      
+      // close context
+      Core.context.close();
+   }
+
+   public void hook (Value script) {
+
+      // add script to hooks
+      if (script.canExecute()) hooks.add(script);
+   }
+
+   public void push (Value script) {
+
+      // add script to queue
       if (script.canExecute()) queue.add(script);
    }
 
-   public void execute (Value script) {
-      if (script.canExecute()) queue.add(Value.asValue((Runnable) () -> new Thread(() -> script.execute()).run()));
+   public void sync (Value script) {
+
+      // add thread to queue
+      if (script.canExecute()) queue.add(Value.asValue(new Thread(script::execute)));
+   }
+
+   public String getRoot () {
+
+      // provide root path
+      return Core.root;
+   }
+
+   public void setRoot (String path) {
+
+      // declare root path
+      Core.root = path;
    }
 }
--- src/main/java/grakkit/Main.java
+++ src/main/java/grakkit/Main.java
@@ -1,38 +1,98 @@
 package grakkit;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.net.URL;
-import java.net.URLClassLoader;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.command.CommandMap;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import org.graalvm.polyglot.Value;
 
-public final class Main extends JavaPlugin {
+class CustomCommand extends Command {
+   
+   public Value executor;
+   public Value tabCompleter;
 
+   public static Value empty = Value.asValue((Runnable) () -> {});
    public static CommandMap registry;
-   public static Map<String, Custom> commands = new HashMap<>();
+   public static Map<String, CustomCommand> commands = new HashMap<>();
+
+   public CustomCommand (String name, String[] aliases, String permission, String message, Value executor, Value tabCompleter) {
+      super(name, "", "", Arrays.asList(aliases));
+      this.executor = executor;
+      this.tabCompleter = tabCompleter;
+      if (permission.length() > 0) {
+         this.setPermission(permission);
+         this.setPermissionMessage(message);
+      }
+   }
+
+   @Override
+   public boolean execute (CommandSender sender, String label, String[] args) {
+
+      // handle errors
+      try {
+
+         // execute script
+         this.executor.execute(sender, label, args);
+      } catch (Exception error) {
+
+         // log error
+         error.printStackTrace(System.err);
+      }
+
+      // report command success to server
+      return true;
+   }
+
+   @Override
+   public List<String> tabComplete (CommandSender sender, String alias, String[] args) {
+
+      // create list
+      List<String> output = Arrays.asList();
+
+      // handle errors
+      try {
+
+         // add tab completions to output
+         Arrays.asList(this.tabCompleter.execute(sender, alias, args)).forEach(value -> output.add(value.toString()));
+      } catch (Exception error) {
+
+         // log error
+         error.printStackTrace(System.err);
+      }
+
+      // send tab completions to server
+      return output;
+   }
+}
+
+public class Main extends JavaPlugin {
 
    public void reload () {
+
+      // disable plugin
       getServer().getPluginManager().disablePlugin(this);
+
+      // enable plugin
       getServer().getPluginManager().enablePlugin(this);
    }
 
-   public void register (String namespace, String name, List<String> aliases, String permission, String message, Value executor, Value tabCompleter) {
+   public static void register (String namespace, String name, String[] aliases, String permission, String message, Value executor, Value tabCompleter) {
 
       // define key
       String key = namespace + ":" + name;
 
       // check if command already exists
-      if (Main.commands.containsKey(key)) {
+      if (CustomCommand.commands.containsKey(key)) {
 
          // modify existing command
-         Custom command = commands.get(key);
+         CustomCommand command = CustomCommand.commands.get(key);
          command.setPermission(permission);
          command.setPermissionMessage(message);
          command.executor = executor;
@@ -40,104 +100,70 @@ public final class Main extends JavaPlugin {
       } else {
 
          // create new command
-         Custom command = new Custom(name, aliases, permission, message, executor, tabCompleter);
-         Main.registry.register(namespace, command);
-         Main.commands.put(key, command);
+         CustomCommand command = new CustomCommand(name, aliases, permission, message, executor, tabCompleter);
+         CustomCommand.registry.register(namespace, command);
+         CustomCommand.commands.put(key, command);
       }
    }
 
    @Override
    public void onLoad() {
 
-      // reflect the registry
+      // handle errors
       try {
+
+         // reflect the registry
          Field internal = getServer().getClass().getDeclaredField("commandMap");
          internal.setAccessible(true);
-         Main.registry = (CommandMap) internal.get(getServer());
+         CustomCommand.registry = (CommandMap) internal.get(getServer());
       } catch (Exception error) {
 
-         // handle errors and exit
+         // log error
          error.printStackTrace(System.err);
+
+         // disable plugin
          this.getServer().getPluginManager().disablePlugin(this);
       }
    }
 
    @Override
    public void onEnable() {
-      try {
 
-         // de-reference executors and tab-completers for each command (doesnt work)
-         Main.commands.values().forEach(command -> {
-            command.executor = null;
-            command.tabCompleter = null;
-         });
+      // handle errors
+      try {
 
          // create plugin folder
          this.getDataFolder().mkdir();
 
-         // copy default config from resources
+         // copy default configuration
          this.getConfig().options().copyDefaults(true);
-
-         // save config
          this.saveDefaultConfig();
 
          // load context
-         Core.load(getDataFolder().getPath(), getConfig().getString("main", "index.js"));
+         Core.open(getDataFolder().getPath(), getConfig().getString("main", "index.js"));
 
          // begin tick loop
          this.getServer().getScheduler().runTaskTimer(this, Core::loop, 0, 1);
       } catch (Exception error) {
 
-         // handle errors and exit
+         // log error
          error.printStackTrace(System.err);
+
+         // disable plugin
          this.getServer().getPluginManager().disablePlugin(this);
       }
    }
 
    @Override
    public void onDisable() {
-   }
 
-   private static URL locate (Class<?> clazz) {
-      try {
-         URL location = clazz.getProtectionDomain().getCodeSource().getLocation();
-         if (location instanceof URL) return location;
-      } catch (SecurityException | NullPointerException error) {
-         // try other method instead of throwing error
-      }
-      URL resource = clazz.getResource(clazz.getSimpleName() + ".class");
-      if (resource instanceof URL) {
-         String link = resource.toString();
-         String suffix = clazz.getCanonicalName().replace('.', '/') + ".class";
-         if (link.endsWith(suffix)) {
-            String base = link.substring(0, link.length() - suffix.length()), path = base;
-            if (path.startsWith("jar:")) path = path.substring(4, path.length() - 2);
-            try {
-               return new URL(path);
-            } catch (Exception error) {
-               return null;
-            }
-         } else {
-            return null;
-         }
-      } else {
-         return null;
-      }
-   }
+      // close context
+      Core.close();
 
-   static {
-      try {
-         Class.forName("org.graalvm.polyglot.Value");
-      } catch (Exception error1) {
-         try {
-            URLClassLoader loader = (URLClassLoader) Thread.currentThread().getContextClassLoader();
-            Class<URLClassLoader> clazz = URLClassLoader.class;
-            Method method = clazz.getDeclaredMethod("addURL", URL.class);
-            method.setAccessible(true);
-            method.invoke(loader, Main.locate(Main.class));
-         } catch (Exception error2) {
-            throw new RuntimeException("Failed to add plugin to class path!", error2);
-         }
-      }
+      // de-reference command scripts
+      CustomCommand.commands.values().forEach(command -> {
+         command.executor = CustomCommand.empty;
+         command.tabCompleter = CustomCommand.empty;
+      });
    }
 }
