a4ee55cb423881c41f47f4d9a3ed2a4c349a954c
hb432 <hb432@protonmail.com>
2020-11-22 14:58:11 -0500

    more small fixes? wohoho

--- src/main/java/grakkit/Main.java
+++ src/main/java/grakkit/Main.java
@@ -60,8 +60,11 @@ class CustomCommand extends Command {
       // handle errors
       try {
 
+         // retrieve tab completions from script
+         Value input = this.tabCompleter.execute(sender, alias, args);
+
          // add tab completions to output
-         Arrays.asList(this.tabCompleter.execute(sender, alias, args)).forEach(value -> output.add(value.toString()));
+         for (long index = 0; index < input.getArraySize(); index++) output.add(input.getArrayElement(index).toString());
       } catch (Exception error) {
 
          // log error
@@ -113,7 +116,7 @@ public class Main extends JavaPlugin {
       // handle errors
       try {
 
-         // reflect the registry
+         // reflect command map
          Field internal = getServer().getClass().getDeclaredField("commandMap");
          internal.setAccessible(true);
          CustomCommand.registry = (CommandMap) internal.get(getServer());
@@ -140,10 +143,10 @@ public class Main extends JavaPlugin {
          this.getConfig().options().copyDefaults(true);
          this.saveDefaultConfig();
 
-         // load context
+         // open context
          Core.open(getDataFolder().getPath(), getConfig().getString("main", "index.js"));
 
-         // begin tick loop
+         // loop the queue
          this.getServer().getScheduler().runTaskTimer(this, Core::loop, 0, 1);
       } catch (Exception error) {
 
