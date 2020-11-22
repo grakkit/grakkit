c6287e10a5c7394d919293fe5648cd8951578cdc
root <root@vmi468422.contaboserver.net>
2020-11-22 04:10:49 +0100

    yo this is QUITE epic

--- src/main/java/grakkit/core.java
+++ src/main/java/grakkit/core.java
@@ -2,8 +2,9 @@ package grakkit;
 
 import java.io.File;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.function.Consumer;
 
 import org.graalvm.polyglot.Context;
@@ -14,27 +15,12 @@ public final class core {
 
    public static Context context;
 
-   public static Set<Value> queue;
+   public static List<Value> queue = new LinkedList<>();
 
    public static Map<String, Value> methods = new HashMap<>();
 
-   static {
-      methods.put("queue", Value.asValue((Consumer<Value>) (script) -> {
-
-         // add script to queue
-         queue.add(script);
-      }));
-      /*
-      methods.put("execute", Value.asValue((Consumer<Value>) (script) -> {
-         
-         // create new thread
-         queue.add(Value.asValue((Runnable) () -> new Thread(() -> script.execute()).run()));
-      }));
-      */
-   }
-
    public static void tick () {
-      queue.forEach(value -> {
+      new LinkedList<Value> (queue).forEach(value -> {
          value.execute();
          queue.remove(value);
       });
@@ -59,7 +45,7 @@ public final class core {
 
          // evaluate index.js
          try {
-            core.context.getBindings("js").putMember("Core", Value.asValue(methods));
+            core.context.getBindings("js").putMember("Core", Value.asValue(new core()));
             core.context.eval(Source.newBuilder("js", index).mimeType("application/javascript+module").cached(false).build());
          } catch (Exception error) {
 
@@ -74,5 +60,13 @@ public final class core {
          // return status
          return false;
       }
+   }
+
+   public void queue (Value script) {
+      queue.add(script);
+   }
+
+   public void execute (Value script) {
+      queue.add(Value.asValue((Runnable) () -> new Thread(() -> script.execute()).run()));
    }
 }
