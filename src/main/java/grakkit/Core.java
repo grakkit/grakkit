07b3ba2d741b110a75d05511fa23ce2fae6479f2
hb432 <hb432@protonmail.com>
2020-11-27 15:02:12 -0500

    apparently that was all wrong

--- src/main/java/grakkit/Core.java
+++ src/main/java/grakkit/Core.java
@@ -12,25 +12,25 @@ import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.Source;
 import org.graalvm.polyglot.Value;
 
-class Superset {
-   
-   /* the internal list */
-   public List<Value> list = new LinkedList<>();
-
-   /** execute and remove all scripts */
-   public void release () {
-      new LinkedList<Value>(this.list).forEach(value -> {
-         try {
-            if (value.canExecute()) value.execute();
-         } catch (Throwable error) {
-            // do nothing
-         }
-         this.list.remove(value);
-      });
-   }
-}
-
 public class Core {
+
+   static class Superset {
+      
+      /* the internal list */
+      public List<Value> list = new LinkedList<>();
+   
+      /** execute and remove all scripts */
+      public void release () {
+         new LinkedList<Value>(this.list).forEach(value -> {
+            try {
+               if (value.canExecute()) value.execute();
+            } catch (Throwable error) {
+               // do nothing
+            }
+            this.list.remove(value);
+         });
+      }
+   }
 
    /** the base path of the environment */
    private static String base;
--- src/main/java/grakkit/Main.java
+++ src/main/java/grakkit/Main.java
@@ -15,87 +15,87 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import org.graalvm.polyglot.Value;
 
-class Wrapper extends Command {
-
-   public Wrapper (String name, String[] aliases, String permission, String message, Value executor, Value tabCompleter) {
-      super(name, "", "", Arrays.asList(aliases));
-      this.executor = executor;
-      this.tabCompleter = tabCompleter;
-      this.setPermission(permission);
-      this.setPermissionMessage(message);
-   }
-   
-   /** the current executor script */
-   public Value executor;
-
-   /** the current tab-completer script */
-   public Value tabCompleter;
-
-   /** the server command map */
-   public static CommandMap registry;
-
-   /** the current list of registered commands */
-   public static Map<String, Wrapper> commands = new HashMap<>();
-
-   /** exposes the server command map with reflection */
-   public static void init (Server server) {
-      try {
-         Field internal = server.getClass().getDeclaredField("commandMap");
-         internal.setAccessible(true);
-         Wrapper.registry = (CommandMap) internal.get(server);
-      } catch (Throwable error) {
-         error.printStackTrace(System.err);
-      }
-   }
-
-   /** registers a new command or updates an existing one */
-   public static void register (String namespace, String name, String[] aliases, String permission, String message, Value executor, Value tabCompleter) {
-      String key = namespace + ":" + name;
-      if (Wrapper.commands.containsKey(key)) {
-         Wrapper command = Wrapper.commands.get(key);
-         command.setPermission(permission);
-         command.setPermissionMessage(message);
-         command.executor = executor;
-         command.tabCompleter = tabCompleter;
-      } else {
-         Wrapper command = new Wrapper(name, aliases, permission, message, executor, tabCompleter);
-         Wrapper.registry.register(namespace, command);
-         Wrapper.commands.put(key, command);
-      }
-   }
-
-   /** unlinks all executors and tab-completers */
-   public static void close () {
-      Wrapper.commands.values().forEach(command -> {
-         command.executor = Value.asValue((Runnable) () -> {});
-         command.tabCompleter = Value.asValue((Runnable) () -> {});
-      });
-   }
-
-   /** runs the current executor script */
-   public boolean execute (CommandSender sender, String label, String[] args) {
-      try {
-         this.executor.execute(sender, label, args);
-      } catch (Throwable error) {
-         error.printStackTrace(System.err);
-      }
-      return true;
-   }
-
-   /** runs and returns the results from the current tab-completer script */
-   public List<String> tabComplete (CommandSender sender, String alias, String[] args) {
-      List<String> output = new ArrayList<String>();
-      try {
-         Value input = this.tabCompleter.execute(sender, alias, args);
-         for (long index = 0; index < input.getArraySize(); index++) output.add(input.getArrayElement(index).toString());
-      } catch (Throwable error) {
-         error.printStackTrace(System.err);
-      }
-      return output;
-   }
-}
-
 public class Main extends JavaPlugin {
+   
+   static class Wrapper extends Command {
+
+      public Wrapper (String name, String[] aliases, String permission, String message, Value executor, Value tabCompleter) {
+         super(name, "", "", Arrays.asList(aliases));
+         this.executor = executor;
+         this.tabCompleter = tabCompleter;
+         this.setPermission(permission);
+         this.setPermissionMessage(message);
+      }
+      
+      /** the current executor script */
+      public Value executor;
+   
+      /** the current tab-completer script */
+      public Value tabCompleter;
+   
+      /** the server command map */
+      public static CommandMap registry;
+   
+      /** the current list of registered commands */
+      public static Map<String, Wrapper> commands = new HashMap<>();
+   
+      /** exposes the server command map with reflection */
+      public static void init (Server server) {
+         try {
+            Field internal = server.getClass().getDeclaredField("commandMap");
+            internal.setAccessible(true);
+            Wrapper.registry = (CommandMap) internal.get(server);
+         } catch (Throwable error) {
+            error.printStackTrace(System.err);
+         }
+      }
+   
+      /** registers a new command or updates an existing one */
+      public static void register (String namespace, String name, String[] aliases, String permission, String message, Value executor, Value tabCompleter) {
+         String key = namespace + ":" + name;
+         if (Wrapper.commands.containsKey(key)) {
+            Wrapper command = Wrapper.commands.get(key);
+            command.setPermission(permission);
+            command.setPermissionMessage(message);
+            command.executor = executor;
+            command.tabCompleter = tabCompleter;
+         } else {
+            Wrapper command = new Wrapper(name, aliases, permission, message, executor, tabCompleter);
+            Wrapper.registry.register(namespace, command);
+            Wrapper.commands.put(key, command);
+         }
+      }
+   
+      /** unlinks all executors and tab-completers */
+      public static void close () {
+         Wrapper.commands.values().forEach(command -> {
+            command.executor = Value.asValue((Runnable) () -> {});
+            command.tabCompleter = Value.asValue((Runnable) () -> {});
+         });
+      }
+   
+      /** runs the current executor script */
+      public boolean execute (CommandSender sender, String label, String[] args) {
+         try {
+            this.executor.execute(sender, label, args);
+         } catch (Throwable error) {
+            error.printStackTrace(System.err);
+         }
+         return true;
+      }
+   
+      /** runs and returns the results from the current tab-completer script */
+      public List<String> tabComplete (CommandSender sender, String alias, String[] args) {
+         List<String> output = new ArrayList<String>();
+         try {
+            Value input = this.tabCompleter.execute(sender, alias, args);
+            for (long index = 0; index < input.getArraySize(); index++) output.add(input.getArrayElement(index).toString());
+         } catch (Throwable error) {
+            error.printStackTrace(System.err);
+         }
+         return output;
+      }
+   }
 
    static {
       Core.patch(); // patch core on class load (core stage 1)
