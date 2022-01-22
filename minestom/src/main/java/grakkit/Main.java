c88c69e4bd5fe03ed3e0273341e183069b0d1197
brayjamin <brayjamindev@gmail.com>
2022-01-22 12:34:27 -0500

    Fixed Maven version inconsistency, bumped Minestom version, fixed Minestom integration AGAIN SO SUSSUSUSUSUSS

--- minestom/src/main/java/grakkit/Main.java
+++ minestom/src/main/java/grakkit/Main.java
@@ -4,23 +4,24 @@ import net.minestom.server.MinecraftServer;
 
 import net.minestom.server.extensions.Extension;
 
+import net.minestom.server.timer.SchedulerManager;
 import net.minestom.server.utils.time.TimeUnit;
 
 public class Main extends Extension {
-
+   public SchedulerManager schedulerManager = MinecraftServer.getSchedulerManager();
    @Override
-   public void initialize() {
+
+   public LoadStatus initialize() {
       Grakkit.patch(new Loader(this.getClass().getClassLoader())); // CORE - patch class loader with GraalJS
+      schedulerManager.buildTask(() -> {
+         schedulerManager.buildTask(() -> {
+            Grakkit.tick();
+         }).repeat(1, TimeUnit.TICK).schedule(); // CORE - run task loop
+         Grakkit.init(this.getDataDirectory().toString()); // CORE - initialize
+      }).delay(50, TimeUnit.MILLISECOND).schedule(); // delay 1/2 a second
+      this.getLogger().info("[Grakkit] Initialized!");
+      return LoadStatus.SUCCESS;
    }
-
-   @Override
-   public void postInitialize() {
-      MinecraftServer.getSchedulerManager().buildTask(() -> {
-         Grakkit.tick();
-      }).repeat(1, TimeUnit.TICK).schedule(); // CORE - run task loop
-      Grakkit.init(this.getDataDirectory().toString()); // CORE - initialize
-   }
-
    @Override
    public void terminate() {
       Grakkit.close(); // CORE - close before exit
